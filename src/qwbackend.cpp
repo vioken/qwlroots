3dc99eda173631eb4b8c27ff1589868a024bf46f
zccrs <zccrs@live.com>
2022-11-14 20:45:32 +0800

    Add signals in QWBackend
    Add newInput and newOutput signals.
    Add QWSignalConnector class to easier to connect a wl_signal, the class
    is copy from Waylib's WSignalconnector, it like as Qt signal-slot
    design.

--- src/qwbackend.cpp
+++ src/qwbackend.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qwbackend.h"
+#include "util/qwsignalconnector.h"
 
 extern "C" {
 #include <wlr/backend.h>
@@ -15,19 +16,42 @@ public:
     QWBackendPrivate(void *handle, QWBackend *qq)
         : QWObjectPrivate(handle, qq)
     {
-
+        sc.connect(&this->handle()->events.new_input, this, &QWBackendPrivate::on_new_input);
+        sc.connect(&this->handle()->events.new_input, this, &QWBackendPrivate::on_new_output);
+        sc.connect(&this->handle()->events.destroy, &sc, &QWSignalConnector::invalidate);
     }
     ~QWBackendPrivate() {
         Q_ASSERT(handle());
         wlr_backend_destroy(handle());
     }
 
+    void on_new_input(void *data);
+    void on_new_output(void *data);
+
     inline wlr_backend *handle() const {
         return q_func()->handle<wlr_backend>();
     }
 
     QW_DECLARE_PUBLIC(QWBackend)
+    QWSignalConnector sc;
 };
+
+
+void QWBackendPrivate::on_new_input(void *data)
+{
+    Q_Q(QWBackend);
+    auto device = reinterpret_cast<wlr_input_device*>(data);
+    Q_ASSERT(device);
+    Q_EMIT q->newInput(device);
+}
+
+void QWBackendPrivate::on_new_output(void *data)
+{
+    Q_Q(QWBackend);
+    auto output = reinterpret_cast<wlr_output*>(data);
+    Q_ASSERT(output);
+    Q_EMIT q->newOutput(output);
+}
 
 QWBackend *QWBackend::autoCreate(wl_display *display, QObject *parent)
 {
---
+++ src/util/qwsignalconnector.h
@@ -0,0 +1,46 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QVector>
+
+struct wl_signal;
+
+QW_BEGIN_NAMESPACE
+
+struct Listener;
+class QW_EXPORT QWSignalConnector {
+public:
+    QWSignalConnector();
+    ~QWSignalConnector();
+
+    typedef void (*SlotFun0)(void *obj);
+    Listener *connect(wl_signal *signal, void *object, SlotFun0 slot);
+    typedef void (*SlotFun1)(void *obj, void *signalData);
+    Listener *connect(wl_signal *signal, void *object, SlotFun1 slot);
+    typedef void (*SlotFun2)(void *obj, void *signalData, void *data);
+    Listener *connect(wl_signal *signal, void *object, SlotFun2 slot, void *data);
+    template <typename T>
+    inline Listener *connect(wl_signal *signal, T *object, void (T::*slot)()) {
+        return connect(signal, object, reinterpret_cast<SlotFun0>(*(void**)(&slot)));
+    }
+    template <typename T>
+    inline Listener *connect(wl_signal *signal, T *object, void (T::*slot)(void*)) {
+        return connect(signal, object, reinterpret_cast<SlotFun1>(*(void**)(&slot)));
+    }
+    template <typename T>
+    inline Listener *connect(wl_signal *signal, T *object, void (T::*slot)(void*, void*), void *data) {
+        return connect(signal, object, reinterpret_cast<SlotFun2>(*(void**)(&slot)), data);
+    }
+    void disconnect(Listener *l);
+    void disconnect(wl_signal *signal);
+    void invalidate();
+    void invalidate(wl_signal *signal);
+
+private:
+    QVector<Listener*> listenerList;
+};
+
+QW_END_NAMESPACE
