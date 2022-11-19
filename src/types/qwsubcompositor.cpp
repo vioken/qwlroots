b6595666b37f9902830caccd0fd2301313a54bc3
zccrs <zccrs@live.com>
2022-11-19 21:36:32 +0800

    Implement QWSubcompositor for wlr_subcompositor

---
+++ src/types/qwsubcompositor.cpp
@@ -0,0 +1,51 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwsubcompositor.h"
+#include "util/qwsignalconnector.h"
+
+extern "C" {
+#include <wlr/types/wlr_subcompositor.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+class QWSubcompositorPrivate : public QWObjectPrivate
+{
+public:
+    QWSubcompositorPrivate(void *handle, QWSubcompositor *qq)
+        : QWObjectPrivate(handle, qq)
+    {
+        sc.connect(&q_func()->handle()->events.destroy, this, &QWSubcompositorPrivate::on_destroy);
+    }
+    ~QWSubcompositorPrivate() {
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+
+    QW_DECLARE_PUBLIC(QWSubcompositor)
+    QWSignalConnector sc;
+};
+
+void QWSubcompositorPrivate::on_destroy(void *)
+{
+    m_handle = nullptr;
+    delete q_func();
+}
+
+QWSubcompositor::QWSubcompositor(wlr_subcompositor *handle)
+    : QWObject(*new QWSubcompositorPrivate(handle, this))
+{
+
+}
+
+QWSubcompositor *QWSubcompositor::create(wl_display *display)
+{
+    auto subcompositor = wlr_subcompositor_create(display);
+    if (!subcompositor)
+        return nullptr;
+    return new QWSubcompositor(subcompositor);
+}
+
+QW_END_NAMESPACE
