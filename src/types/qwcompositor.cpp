5b1c5d347869fe8d05f30b0ab3d1cc54040bba8c
zccrs <zccrs@live.com>
2022-11-19 20:28:40 +0800

    Implement QWCompositor for wlr_compositor

---
+++ src/types/qwcompositor.cpp
@@ -0,0 +1,60 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwcompositor.h"
+#include "util/qwsignalconnector.h"
+#include "render/qwrenderer.h"
+
+extern "C" {
+#include <wlr/types/wlr_compositor.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+class QWCompositorPrivate : public QWObjectPrivate
+{
+public:
+    QWCompositorPrivate(void *handle, QWCompositor *qq)
+        : QWObjectPrivate(handle, qq)
+    {
+        sc.connect(&q_func()->handle()->events.destroy, this, &QWCompositorPrivate::on_destroy);
+        sc.connect(&q_func()->handle()->events.new_surface, this, &QWCompositorPrivate::on_new_surface);
+    }
+    ~QWCompositorPrivate() {
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+    void on_new_surface(void *data);
+
+    QW_DECLARE_PUBLIC(QWCompositor)
+    QWSignalConnector sc;
+};
+
+void QWCompositorPrivate::on_destroy(void *)
+{
+    m_handle = nullptr;
+    q_func()->deleteLater();
+}
+
+void QWCompositorPrivate::on_new_surface(void *data)
+{
+    Q_EMIT q_func()->newSurface(reinterpret_cast<wlr_surface*>(data));
+}
+
+QWCompositor::QWCompositor(wlr_compositor *handle)
+    : QWObject(*new QWCompositorPrivate(handle, this))
+    , QObject(nullptr)
+{
+
+}
+
+QWCompositor *QWCompositor::create(wl_display *display, QWRenderer *renderer)
+{
+    auto compositor = wlr_compositor_create(display, renderer->handle());
+    if (!compositor)
+        return nullptr;
+    return new QWCompositor(compositor);
+}
+
+QW_END_NAMESPACE
---
+++ src/types/qwcompositor.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QObject>
+
+struct wl_display;
+struct wlr_compositor;
+struct wlr_surface;
+
+QW_BEGIN_NAMESPACE
+
+class QWRenderer;
+class QWCompositorPrivate;
+class QW_EXPORT QWCompositor : public QObject, public QWObject
+{
+    QW_DECLARE_PRIVATE(QWCompositor)
+public:
+    explicit QWCompositor(wlr_compositor *handle);
+
+    static QWCompositor *create(wl_display *display, QWRenderer *renderer);
+
+    inline wlr_compositor *handle() const {
+        return QWObject::handle<wlr_compositor>();
+    }
+
+Q_SIGNALS:
+    // TODO: make to QWSurface
+    void newSurface(wlr_surface *surface);
+
+private:
+    ~QWCompositor() = default;
+    using QObject::deleteLater;
+};
+
+QW_END_NAMESPACE
