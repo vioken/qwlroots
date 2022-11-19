3db0dcc6e874a2fe05ca6b56fa35959e747bb067
zccrs <zccrs@live.com>
2022-11-19 21:36:32 +0800

    Implement QWSubcompositor for wlr_subcompositor

---
+++ src/types/qwsubcompositor.h
@@ -0,0 +1,31 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+
+struct wl_display;
+struct wlr_subcompositor;
+struct wlr_subsurface;
+
+QW_BEGIN_NAMESPACE
+
+class QWSubcompositorPrivate;
+class QW_EXPORT QWSubcompositor : public QWObject
+{
+    QW_DECLARE_PRIVATE(QWSubcompositor)
+public:
+    explicit QWSubcompositor(wlr_subcompositor *handle);
+
+    static QWSubcompositor *create(wl_display *display);
+
+    inline wlr_subcompositor *handle() const {
+        return QWObject::handle<wlr_subcompositor>();
+    }
+
+private:
+    ~QWSubcompositor() = default;
+};
+
+QW_END_NAMESPACE
