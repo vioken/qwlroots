89324dff73db828ecf0820fd5f14fe266e6616ef
zccrs <zccrs@live.com>
2022-11-28 21:24:07 +0800

    Implement QWOutput for wlr_output

---
+++ src/types/qwoutput.cpp
@@ -0,0 +1,352 @@
+// Copyright (C) 2022 zccrs zccrs@live.com.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwoutput.h"
+#include "qwbuffer.h"
+#include "util/qwsignalconnector.h"
+#include "render/qwallocator.h"
+#include "render/qwrenderer.h"
+
+#include <QImage>
+#include <QSize>
+
+extern "C" {
+#include <wlr/types/wlr_output.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+class QWOutputPrivate : public QWObjectPrivate
+{
+public:
+    QWOutputPrivate(void *handle, QWOutput *qq)
+        : QWObjectPrivate(handle, qq)
+    {
+        sc.connect(&qq->handle()->events.frame, this, &QWOutputPrivate::on_frame);
+        sc.connect(&qq->handle()->events.damage, this, &QWOutputPrivate::on_damage);
+        sc.connect(&qq->handle()->events.needs_frame, this, &QWOutputPrivate::on_needs_frame);
+        sc.connect(&qq->handle()->events.precommit, this, &QWOutputPrivate::on_precommit);
+        sc.connect(&qq->handle()->events.commit, this, &QWOutputPrivate::on_commit);
+        sc.connect(&qq->handle()->events.present, this, &QWOutputPrivate::on_present);
+        sc.connect(&qq->handle()->events.bind, this, &QWOutputPrivate::on_bind);
+        sc.connect(&qq->handle()->events.enable, this, &QWOutputPrivate::on_enable);
+        sc.connect(&qq->handle()->events.mode, this, &QWOutputPrivate::on_mode);
+        sc.connect(&qq->handle()->events.description, this, &QWOutputPrivate::on_description);
+        sc.connect(&qq->handle()->events.destroy, this, &QWOutputPrivate::on_destroy);
+    }
+    ~QWOutputPrivate() {
+        sc.invalidate();
+        if (m_handle)
+            wlr_output_destroy(q_func()->handle());
+    }
+
+    void on_frame(void *data);
+    void on_damage(void *data);
+    void on_needs_frame(void *data);
+    void on_precommit(void *data);
+    void on_commit(void *data);
+    void on_present(void *data);
+    void on_bind(void *data);
+    void on_enable(void *data);
+    void on_mode(void *data);
+    void on_description(void *data);
+    void on_destroy(void *data);
+
+    QW_DECLARE_PUBLIC(QWOutput)
+    QWSignalConnector sc;
+};
+
+void QWOutputPrivate::on_frame(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->frame();
+}
+
+void QWOutputPrivate::on_damage(void *data)
+{
+    Q_EMIT q_func()->damage(reinterpret_cast<wlr_output_event_damage*>(data));
+}
+
+void QWOutputPrivate::on_needs_frame(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->needsFrame();
+}
+
+void QWOutputPrivate::on_precommit(void *data)
+{
+    Q_EMIT q_func()->precommit(reinterpret_cast<wlr_output_event_precommit*>(data));
+}
+
+void QWOutputPrivate::on_commit(void *data)
+{
+    Q_EMIT q_func()->commit(reinterpret_cast<wlr_output_event_commit*>(data));
+}
+
+void QWOutputPrivate::on_present(void *data)
+{
+    Q_EMIT q_func()->present(reinterpret_cast<wlr_output_event_present*>(data));
+}
+
+void QWOutputPrivate::on_bind(void *data)
+{
+    Q_EMIT q_func()->bind(reinterpret_cast<wlr_output_event_bind*>(data));
+}
+
+void QWOutputPrivate::on_enable(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->enableChanged();
+}
+
+void QWOutputPrivate::on_mode(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->modeChanged();
+}
+
+void QWOutputPrivate::on_description(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->descriptionChanged();
+}
+
+void QWOutputPrivate::on_destroy(void *)
+{
+    m_handle = nullptr;
+    q_func()->deleteLater();
+}
+
+QWOutput::QWOutput(wlr_output *handle)
+    : QObject(nullptr)
+    , QWObject(*new QWOutputPrivate(handle, this))
+{
+
+}
+
+wlr_output *QWOutput::fromResource(wl_resource *resource)
+{
+    return wlr_output_from_resource(resource);
+}
+
+void QWOutput::enable(bool enable)
+{
+    wlr_output_enable(handle(), enable);
+}
+
+void QWOutput::createGlobal()
+{
+    wlr_output_create_global(handle());
+}
+
+void QWOutput::destroyGlobal()
+{
+    wlr_output_destroy_global(handle());
+}
+
+bool QWOutput::initRender(QWAllocator *allocator, QWRenderer *renderer)
+{
+    return wlr_output_init_render(handle(), allocator->handle(), renderer->handle());
+}
+
+wlr_output_mode *QWOutput::preferredMode() const
+{
+    return wlr_output_preferred_mode(handle());
+}
+
+void QWOutput::setMode(wlr_output_mode *mode)
+{
+    wlr_output_set_mode(handle(), mode);
+}
+
+void QWOutput::setCustomMode(const QSize &size, int32_t refresh)
+{
+    wlr_output_set_custom_mode(handle(), size.width(), size.height(), refresh);
+}
+
+void QWOutput::setTransform(int transform)
+{
+    wlr_output_set_transform(handle(), static_cast<wl_output_transform>(transform));
+}
+
+void QWOutput::enableAdaptiveSync(bool enabled)
+{
+    wlr_output_enable_adaptive_sync(handle(), enabled);
+}
+
+void QWOutput::setRenderFormat(uint32_t format)
+{
+    wlr_output_set_render_format(handle(), format);
+}
+
+void QWOutput::setScale(float scale)
+{
+    wlr_output_set_scale(handle(), scale);
+}
+
+void QWOutput::setSubpixel(int subpixel)
+{
+    wlr_output_set_subpixel(handle(), static_cast<wl_output_subpixel>(subpixel));
+}
+
+void QWOutput::setName(const QByteArray &name)
+{
+    wlr_output_set_name(handle(), name.constData());
+}
+
+void QWOutput::setDescription(const QByteArray &desc)
+{
+    wlr_output_set_description(handle(), desc.constData());
+}
+
+void QWOutput::scheduleDone()
+{
+    wlr_output_schedule_done(handle());
+}
+
+QSize QWOutput::transformedResolution() const
+{
+    QSize size;
+    wlr_output_transformed_resolution(handle(), &size.rwidth(), &size.rheight());
+    return size;
+}
+
+QSize QWOutput::effectiveResolution() const
+{
+    QSize size;
+    wlr_output_effective_resolution(handle(), &size.rwidth(), &size.rheight());
+    return size;
+}
+
+bool QWOutput::attachRender(int *bufferAge)
+{
+    return wlr_output_attach_render(handle(), bufferAge);
+}
+
+void QWOutput::lockAttachRender(bool lock)
+{
+    wlr_output_lock_attach_render(handle(), lock);
+}
+
+void QWOutput::attachBuffer(QWBuffer *buffer)
+{
+    wlr_output_attach_buffer(handle(), buffer->handle());
+}
+
+uint32_t QWOutput::preferredReadFormat() const
+{
+    return wlr_output_preferred_read_format(handle());
+}
+
+void QWOutput::setDamage(pixman_region32 *damage)
+{
+    wlr_output_set_damage(handle(), damage);
+}
+
+bool QWOutput::test()
+{
+    return wlr_output_test(handle());
+}
+
+bool QWOutput::commit()
+{
+    return wlr_output_commit(handle());
+}
+
+void QWOutput::rollback()
+{
+    wlr_output_rollback(handle());
+}
+
+bool QWOutput::testState(wlr_output_state *state)
+{
+    return wlr_output_test_state(handle(), state);
+}
+
+bool QWOutput::commitState(wlr_output_state *state)
+{
+    return wlr_output_commit_state(handle(), state);
+}
+
+void QWOutput::scheduleFrame()
+{
+    wlr_output_schedule_frame(handle());
+}
+
+size_t QWOutput::getGammaSize() const
+{
+    return wlr_output_get_gamma_size(handle());
+}
+
+void QWOutput::setGamma(size_t size, const uint16_t *r, const uint16_t *g, const uint16_t *b)
+{
+    wlr_output_set_gamma(handle(), size, r, g, b);
+}
+
+void QWOutput::lockSoftwareCursors(bool lock)
+{
+    wlr_output_lock_software_cursors(handle(), lock);
+}
+
+void QWOutput::renderSoftwareCursors(pixman_region32 *damage)
+{
+    wlr_output_render_software_cursors(handle(), damage);
+}
+
+const wlr_drm_format_set *QWOutput::getPrimaryFormats(uint32_t bufferCaps)
+{
+    return wlr_output_get_primary_formats(handle(), bufferCaps);
+}
+
+class QWOutputCursorPrivate : public QWObjectPrivate
+{
+public:
+    QWOutputCursorPrivate(void *handle, QWOutputCursor *qq)
+        : QWObjectPrivate(handle, qq)
+    {
+
+    }
+    ~QWOutputCursorPrivate() {
+        wlr_output_cursor_destroy(q_func()->handle());
+    }
+
+    QW_DECLARE_PUBLIC(QWOutputCursor)
+};
+
+QWOutputCursor::QWOutputCursor(wlr_output_cursor *handle)
+    : QWObject(*new QWOutputCursorPrivate(handle, this))
+{
+
+}
+
+QWOutputCursor *QWOutputCursor::create(QWOutput *output)
+{
+    auto handle = wlr_output_cursor_create(output->handle());
+    if (!handle)
+        return nullptr;
+    return new QWOutputCursor(handle);
+}
+
+bool QWOutputCursor::setImage(const QImage &image, const QPoint &hotspot)
+{
+    return wlr_output_cursor_set_image(handle(), reinterpret_cast<const uint8_t*>(image.constBits()),
+                                       image.bitPlaneCount(), image.width(), image.height(),
+                                       hotspot.x(), hotspot.y());
+}
+
+void QWOutputCursor::setSurface(wlr_surface *surface, const QPoint &hotspot)
+{
+    wlr_output_cursor_set_surface(handle(), surface, hotspot.x(), hotspot.y());
+}
+
+bool QWOutputCursor::setBuffer(QWBuffer *buffer, const QPoint &hotspot)
+{
+    return wlr_output_cursor_set_buffer(handle(), buffer->handle(), hotspot.x(), hotspot.y());
+}
+
+bool QWOutputCursor::move(const QPointF &pos)
+{
+    return wlr_output_cursor_move(handle(), pos.x(), pos.y());
+}
+
+QW_END_NAMESPACE
