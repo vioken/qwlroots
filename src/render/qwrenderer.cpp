66636b30ee379df9b8a390ac337386204d106fa9
JiDe Zhang <zccrs@live.com>
2022-11-30 20:11:19 +0800

    Update the copyright
    From "zccrs zccrs@live.com" to "JiDe Zhang <zccrs@live.com>".

--- src/render/qwrenderer.cpp
+++ src/render/qwrenderer.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2022 zccrs zccrs@live.com.
+// Copyright (C) 2022 JiDe Zhang <zccrs@live.com>.
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qwrenderer.h"
