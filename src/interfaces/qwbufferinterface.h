87fcdee7b028e9be4abd754d8dd5e946b7010f74
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-05-24 16:27:09 +0800

    Implement QWBufferInterface from wlr_buffer

---
+++ src/interfaces/qwbufferinterface.h
@@ -0,0 +1,46 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwinterface.h>
+
+struct wlr_buffer;
+struct wlr_buffer_impl;
+struct wlr_dmabuf_attributes;
+struct wlr_shm_attributes;
+
+class QWBuffer;
+
+QW_BEGIN_NAMESPACE
+
+class QWBufferInterface : public QWInterface
+{
+public:
+    virtual ~QWBufferInterface();
+
+    virtual bool getDmabuf(wlr_dmabuf_attributes *attribs) const;
+    virtual bool getShm(wlr_shm_attributes *attribs) const;
+    virtual bool beginDataPtrAccess(uint32_t flags, void **data, uint32_t *format, size_t *stride);
+    virtual void endDataPtrAccess();
+
+    inline wlr_buffer *handle() const { return QWInterface::handle<wlr_buffer>(); }
+    inline wlr_buffer_impl *impl() const { return QWInterface::handle<wlr_buffer_impl>(); }
+    static QWBufferInterface *get(wlr_buffer *handle);
+
+protected:
+    template<class T>
+    inline void init(int width, int height)
+    {
+        init(getFuncMagicKey<T>(&T::getDmabuf,
+                                &T::getShm,
+                                &T::beginDataPtrAccess,
+                                &T::endDataPtrAccess),
+             width,
+             height);
+    }
+
+    virtual void init(FuncMagicKey funMagicKey, int width, int height);
+};
+
+QW_END_NAMESPACE
