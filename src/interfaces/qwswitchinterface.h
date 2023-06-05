cafc3acd0db11027e12aff1985cc5e4a4bfff0cf
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-06-05 13:43:05 +0800

    Implement QWSwitchInterface from wlr_switch_impl

---
+++ src/interfaces/qwswitchinterface.h
@@ -0,0 +1,34 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwinterface.h>
+
+struct wlr_switch;
+struct wlr_switch_impl;
+
+QW_BEGIN_NAMESPACE
+
+class QWSwitchInterface : public QWInterface {
+public:
+    virtual const char* name() = 0;
+
+    inline wlr_switch *handle() const {
+        return QWInterface::handle<wlr_switch>();
+    }
+
+protected:
+    void notifyToggle() const;
+
+    // TODO: Add create function in QWSwitch
+    template<class T>
+    inline void init(const char* name)
+    {
+        init(getFuncMagicKey<T>(), name);
+    }
+
+    virtual void init(FuncMagicKey funMagicKey, const char* name);
+};
+
+QW_END_NAMESPACE
