7b8b1a6aa334b9c2e58de84a3a25945f441930b1
rewine <lhongxu@outlook.com>
2023-06-15 16:58:01 +0800

    Implement QWInputInhibitManager from wlr_input_inhibit_manager

---
+++ src/types/qwinputinhibitmanager.cpp
@@ -0,0 +1,97 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwinputinhibitmanager.h"
+#include "util/qwsignalconnector.h"
+
+#include <QHash>
+
+extern "C" {
+#include <wlr/types/wlr_input_inhibitor.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+class QWInputInhibitManagerPrivate : public QWObjectPrivate
+{
+public:
+    QWInputInhibitManagerPrivate(wlr_input_inhibit_manager *handle, bool isOwner, QWInputInhibitManager *qq)
+        : QWObjectPrivate(handle, isOwner, qq)
+    {
+        Q_ASSERT(!map.contains(handle));
+        map.insert(handle, qq);
+        sc.connect(&handle->events.destroy, this, &QWInputInhibitManagerPrivate::on_destroy);
+        sc.connect(&handle->events.activate, this, &QWInputInhibitManagerPrivate::on_activate);
+        sc.connect(&handle->events.deactivate, this, &QWInputInhibitManagerPrivate::on_deactivate);
+    }
+    ~QWInputInhibitManagerPrivate() {
+        if (!m_handle)
+            return;
+        destroy();
+    }
+
+    inline void destroy() {
+        Q_ASSERT(m_handle);
+        Q_ASSERT(map.contains(m_handle));
+        Q_EMIT q_func()->beforeDestroy(q_func());
+        map.remove(m_handle);
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+    void on_activate(void *);
+    void on_deactivate(void *);
+
+    static QHash<void*, QWInputInhibitManager*> map;
+    QW_DECLARE_PUBLIC(QWInputInhibitManager)
+    QWSignalConnector sc;
+};
+QHash<void*, QWInputInhibitManager*> QWInputInhibitManagerPrivate::map;
+
+void QWInputInhibitManagerPrivate::on_destroy(void *)
+{
+    destroy();
+    m_handle = nullptr;
+    delete q_func();
+}
+
+void QWInputInhibitManagerPrivate::on_activate(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->activate();
+}
+
+void QWInputInhibitManagerPrivate::on_deactivate(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->deactivate();
+}
+
+QWInputInhibitManager::QWInputInhibitManager(wlr_input_inhibit_manager *handle, bool isOwner)
+    : QObject(nullptr)
+    , QWObject(*new QWInputInhibitManagerPrivate(handle, isOwner, this))
+{
+
+}
+
+QWInputInhibitManager *QWInputInhibitManager::get(wlr_input_inhibit_manager *handle)
+{
+    return QWInputInhibitManagerPrivate::map.value(handle);
+}
+
+ QWInputInhibitManager *QWInputInhibitManager::from(wlr_input_inhibit_manager *handle)
+{
+    if (auto o = get(handle))
+        return o;
+    return new QWInputInhibitManager(handle, false);
+}
+
+QWInputInhibitManager *QWInputInhibitManager::create(wl_display *display)
+{
+    auto *handle = wlr_input_inhibit_manager_create(display);
+    if (!handle)
+        return nullptr;
+    return new QWInputInhibitManager(handle, true);
+}
+
+QW_END_NAMESPACE
---
+++ src/types/qwinputinhibitmanager.h
@@ -0,0 +1,38 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QObject>
+
+struct wlr_input_inhibit_manager;
+struct wl_display;
+
+QW_BEGIN_NAMESPACE
+
+class QWInputInhibitManagerPrivate;
+class QW_EXPORT QWInputInhibitManager : public QObject, public QWObject
+{
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWInputInhibitManager)
+public:
+    inline wlr_input_inhibit_manager *handle() const {
+        return QWObject::handle<wlr_input_inhibit_manager>();
+    }
+
+    static QWInputInhibitManager *get(wlr_input_inhibit_manager *handle);
+    static QWInputInhibitManager *from(wlr_input_inhibit_manager *handle);
+    static QWInputInhibitManager *create(wl_display *display);
+
+Q_SIGNALS:
+    void beforeDestroy(QWInputInhibitManager *self);
+    void activate();
+    void deactivate();
+
+private:
+    QWInputInhibitManager(wlr_input_inhibit_manager *handle, bool isOwner);
+};
+
+QW_END_NAMESPACE
+
