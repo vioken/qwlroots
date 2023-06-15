e50059095f954e1f876ea51c78d7d2e7a953fca6
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-06-15 13:11:36 +0800

    Implement QWXWaylandSurface from wlr_xwayland_surface

---
+++ src/types/qwxwaylandsurface.cpp
@@ -0,0 +1,92 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwxwaylandsurface.h"
+
+#include <QRect>
+
+extern "C" {
+#include <xcb/xproto.h>
+#include <math.h>
+#define class _class
+#include <wlr/xwayland/xwayland.h>
+#undef class
+}
+
+QW_BEGIN_NAMESPACE
+
+QWXWaylandSurface* QWXWaylandSurface::from(wlr_xwayland_surface* surface)
+{
+    return reinterpret_cast<QWXWaylandSurface*>(surface);
+}
+
+wlr_xwayland_surface* QWXWaylandSurface::handle() const
+{
+    return reinterpret_cast<wlr_xwayland_surface*>(const_cast<QWXWaylandSurface*>(this));
+}
+
+void QWXWaylandSurface::activate(bool activated)
+{
+    wlr_xwayland_surface_activate(handle(), activated);
+}
+
+void QWXWaylandSurface::restack(QWXWaylandSurface *sibling, xcb_stack_mode mode)
+{
+    wlr_xwayland_surface_restack(handle(), sibling->handle(), static_cast<xcb_stack_mode_t>(mode));
+}
+
+void QWXWaylandSurface::configure(const QRect &rect)
+{
+    wlr_xwayland_surface_configure(handle(), rect.x(), rect.y(), rect.width(), rect.height());
+}
+
+void QWXWaylandSurface::close()
+{
+    wlr_xwayland_surface_close(handle());
+}
+
+#if WLR_VERSION_MAJOR == 0 && WLR_VERSION_MINOR > 16
+void QWXWaylandSurface::setWithdrawn(bool withdrawn)
+{
+    wlr_xwayland_surface_set_withdrawn(handle(), withdrawn);
+}
+#endif
+
+void QWXWaylandSurface::setMinimized(bool minimized)
+{
+    wlr_xwayland_surface_set_minimized(handle(), minimized);
+}
+
+void QWXWaylandSurface::setMaximized(bool maximized)
+{
+    wlr_xwayland_surface_set_maximized(handle(), maximized);
+}
+
+void QWXWaylandSurface::setFullscreen(bool fullscreen)
+{
+    wlr_xwayland_surface_set_fullscreen(handle(), fullscreen);
+}
+
+#if WLR_VERSION_MAJOR == 0 && WLR_VERSION_MINOR > 16
+QWXWaylandSurface *QWXWaylandSurface::tryFromWlrSurface(QWSurface *surface)
+{
+    return from(wlr_xwayland_surface_try_from_wlr_surface(surface->handle()));
+}
+#endif
+
+void QWXWaylandSurface::ping()
+{
+    wlr_xwayland_surface_ping(handle());
+}
+
+bool QWXWaylandSurface::xwaylandOrSurfaceWantsFocus() const
+{
+    return wlr_xwayland_or_surface_wants_focus(handle());
+}
+
+wlr_xwayland_icccm_input_model_t QWXWaylandSurface::icccmInputModel() const
+{
+    return wlr_xwayland_icccm_input_model(handle());
+}
+
+QW_END_NAMESPACE
---
+++ src/types/qwxwaylandsurface.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR
+// GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+
+typedef int wlr_xwayland_icccm_input_model_t;
+typedef int xcb_stack_mode;
+
+struct wlr_xwayland_surface;
+class QRect;
+
+QW_BEGIN_NAMESPACE
+
+class QWSurface;
+class QWXWaylandSurface {
+public:
+    static QWXWaylandSurface* from(wlr_xwayland_surface* surface);
+#if WLR_VERSION_MAJOR == 0 && WLR_VERSION_MINOR > 16
+    static QWXWaylandSurface *tryFromWlrSurface(QWSurface *surface);
+#endif
+
+    wlr_xwayland_surface* handle() const;
+    void activate(bool activated);
+    void restack(QWXWaylandSurface *sibling, xcb_stack_mode mode);
+    void configure(const QRect &rect);
+    void close();
+#if WLR_VERSION_MAJOR == 0 && WLR_VERSION_MINOR > 16
+    void setWithdrawn(bool withdrawn);
+#endif
+    void setMinimized(bool minimized);
+    void setMaximized(bool maximized);
+    void setFullscreen(bool fullscreen);
+    void ping();
+    bool xwaylandOrSurfaceWantsFocus() const;
+    wlr_xwayland_icccm_input_model_t icccmInputModel() const;
+};
+
+QW_END_NAMESPACE
