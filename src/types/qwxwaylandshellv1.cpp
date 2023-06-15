a48de6c1c137788f07c62f5f38b0d529f787389b
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-06-15 14:53:16 +0800

    Implement QWXWaylandShellV1 from wlr_xwayland_shell_v1

---
+++ src/types/qwxwaylandshellv1.cpp
@@ -0,0 +1,86 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwxwaylandshellv1.h"
+
+#include <qwglobal.h>
+#include <qwdisplay.h>
+#include <qwcompositor.h>
+#include <util/qwsignalconnector.h>
+
+#include <QHash>
+
+extern "C" {
+#include <wlr/xwayland/shell.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+class QWXWaylandShellV1Private : public QWObjectPrivate {
+public:
+    QWXWaylandShellV1Private(wlr_xwayland_shell_v1* handle, bool isOwner, QWXWaylandShellV1* qq)
+        : QWObjectPrivate(handle, isOwner, qq)
+    {
+        Q_ASSERT(!map.contains(handle));
+        map.insert(handle, qq);
+        sc.connect(&handle->events.new_surface, q_func(), &QWXWaylandShellV1::newSurface);
+    }
+
+    ~QWXWaylandShellV1Private() {
+        if (!m_handle) {
+            return;
+        }
+        destroy();
+        if (isHandleOwner) {
+            wlr_xwayland_shell_v1_destroy(q_func()->handle());
+        }
+    }
+
+    inline void destroy() {
+        Q_ASSERT(m_handle);
+        Q_ASSERT(map.contains(m_handle));
+        Q_EMIT q_func()->beforeDestroy(q_func());
+        map.remove(m_handle);
+        sc.invalidate();
+    }
+
+    static QHash<void*, QWXWaylandShellV1*> map;
+    QW_DECLARE_PUBLIC(QWXWaylandShellV1)
+    QWSignalConnector sc;
+};
+QHash<void*, QWXWaylandShellV1*> QWXWaylandShellV1Private::map;
+
+QWXWaylandShellV1* QWXWaylandShellV1::get(wlr_xwayland_shell_v1* handle)
+{
+    return QWXWaylandShellV1Private::map.value(handle);
+}
+
+QWXWaylandShellV1* QWXWaylandShellV1::create(QWDisplay* display, uint32_t version, QObject* parent)
+{
+    auto* handle = wlr_xwayland_shell_v1_create(display->handle(), version);
+    return handle ? new QWXWaylandShellV1(handle, true, parent) : nullptr;
+}
+
+wlr_xwayland_shell_v1* QWXWaylandShellV1::handle() const
+{
+    return QWObject::handle<wlr_xwayland_shell_v1>();
+}
+
+void QWXWaylandShellV1::setClient(wl_client* client)
+{
+    wlr_xwayland_shell_v1_set_client(handle(), client);
+}
+
+QWSurface *QWXWaylandShellV1::surfaceFromSerial(uint64_t serial) const
+{
+    auto* surface = wlr_xwayland_shell_v1_surface_from_serial(handle(), serial);
+    return surface ? QWSurface::from(surface) : nullptr;
+}
+
+QWXWaylandShellV1::QWXWaylandShellV1(wlr_xwayland_shell_v1* handle, bool isOwner, QObject* parent)
+    : QObject(parent)
+    , QWObject(*new QWXWaylandShellV1Private(handle, isOwner, this))
+{
+}
+
+QW_END_NAMESPACE
---
+++ src/types/qwxwaylandshellv1.h
@@ -0,0 +1,45 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QObject>
+
+QT_BEGIN_NAMESPACE
+
+class QImage;
+class QPoint;
+
+QT_END_NAMESPACE
+
+struct wlr_xwayland_shell_v1;
+struct wl_client;
+
+QW_BEGIN_NAMESPACE
+
+class QWSurface;
+class QWDisplay;
+class QWXWaylandShellV1Private;
+class QW_EXPORT QWXWaylandShellV1 : public QObject, public QWObject
+{
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWXWaylandShellV1)
+public:
+    QWXWaylandShellV1(wlr_xwayland_shell_v1 *handle, bool isOwner = false, QObject *parent = nullptr);
+    ~QWXWaylandShellV1() = default;
+
+    static QWXWaylandShellV1 *create(QWDisplay *display, uint32_t version, QObject *parent = nullptr);
+    static QWXWaylandShellV1 *get(wlr_xwayland_shell_v1 *handle);
+
+    wlr_xwayland_shell_v1 *handle() const;
+
+    void setClient(wl_client *client);
+    QWSurface *surfaceFromSerial(uint64_t serial) const;
+
+Q_SIGNALS:
+    void beforeDestroy(QWXWaylandShellV1 *self);
+    void newSurface();
+};
+
+QW_END_NAMESPACE
