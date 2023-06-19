a7a024e211923a9cd76dad5c35a03d6f5cbc028e
rewine <lhongxu@outlook.com>
2023-06-19 13:50:01 +0800

    Implement QWIdleInhibitorV1/QWIdleInhibitManagerV1

---
+++ src/types/qwidleinhibitv1.cpp
@@ -0,0 +1,153 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#include "qwidleinhibitv1.h"
+#include "qwdisplay.h"
+#include "util/qwsignalconnector.h"
+
+#include <QHash>
+
+extern "C" {
+#include <wlr/types/wlr_idle_inhibit_v1.h>
+}
+
+QW_BEGIN_NAMESPACE
+
+/// QWIdleInhibitManagerV1
+
+class QWIdleInhibitManagerV1Private : public QWObjectPrivate
+{
+public:
+    QWIdleInhibitManagerV1Private(wlr_idle_inhibit_manager_v1 *handle, bool isOwner, QWIdleInhibitManagerV1 *qq)
+        : QWObjectPrivate(handle, isOwner, qq)
+    {
+        Q_ASSERT(!map.contains(handle));
+        map.insert(handle, qq);
+        sc.connect(&handle->events.destroy, this, &QWIdleInhibitManagerV1Private::on_destroy);
+        sc.connect(&handle->events.new_inhibitor, this, &QWIdleInhibitManagerV1Private::on_new_inhibitor);
+    }
+    ~QWIdleInhibitManagerV1Private() {
+        if (!m_handle)
+            return;
+        destroy();
+    }
+
+    inline void destroy() {
+        Q_ASSERT(m_handle);
+        Q_ASSERT(map.contains(m_handle));
+        Q_EMIT q_func()->beforeDestroy(q_func());
+        map.remove(m_handle);
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+    void on_new_inhibitor(void *);
+
+    static QHash<void*, QWIdleInhibitManagerV1*> map;
+    QW_DECLARE_PUBLIC(QWIdleInhibitManagerV1)
+    QWSignalConnector sc;
+};
+QHash<void*, QWIdleInhibitManagerV1*> QWIdleInhibitManagerV1Private::map;
+
+void QWIdleInhibitManagerV1Private::on_destroy(void *)
+{
+    destroy();
+    m_handle = nullptr;
+    delete q_func();
+}
+
+void QWIdleInhibitManagerV1Private::on_new_inhibitor(void *data)
+{
+    Q_ASSERT(m_handle == data);
+    Q_EMIT q_func()->newInhibitor();
+}
+
+QWIdleInhibitManagerV1::QWIdleInhibitManagerV1(wlr_idle_inhibit_manager_v1 *handle, bool isOwner)
+    : QObject(nullptr)
+    , QWObject(*new QWIdleInhibitManagerV1Private(handle, isOwner, this))
+{
+
+}
+
+QWIdleInhibitManagerV1 *QWIdleInhibitManagerV1::get(wlr_idle_inhibit_manager_v1 *handle)
+{
+    return QWIdleInhibitManagerV1Private::map.value(handle);
+}
+
+QWIdleInhibitManagerV1 *QWIdleInhibitManagerV1::from(wlr_idle_inhibit_manager_v1 *handle)
+{
+    if (auto o = get(handle))
+        return o;
+    return new QWIdleInhibitManagerV1(handle, false);
+}
+
+QWIdleInhibitManagerV1 *QWIdleInhibitManagerV1::create(QWDisplay *display)
+{
+    auto *handle = wlr_idle_inhibit_v1_create(display->handle());
+    if (!handle)
+        return nullptr;
+    return new QWIdleInhibitManagerV1(handle, true);
+}
+
+/// QWIdleInhibitManagerV1
+
+class QWIdleInhibitorV1Private : public QWObjectPrivate
+{
+public:
+    QWIdleInhibitorV1Private(wlr_idle_inhibitor_v1 *handle, bool isOwner, QWIdleInhibitorV1 *qq)
+        : QWObjectPrivate(handle, isOwner, qq)
+    {
+        Q_ASSERT(!map.contains(handle));
+        map.insert(handle, qq);
+        sc.connect(&handle->events.destroy, this, &QWIdleInhibitorV1Private::on_destroy);
+    }
+    ~QWIdleInhibitorV1Private() {
+        if (!m_handle)
+            return;
+        destroy();
+    }
+
+    inline void destroy() {
+        Q_ASSERT(m_handle);
+        Q_ASSERT(map.contains(m_handle));
+        Q_EMIT q_func()->beforeDestroy(q_func());
+        map.remove(m_handle);
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+    void on_new_inhibitor(void *);
+
+    static QHash<void*, QWIdleInhibitorV1*> map;
+    QW_DECLARE_PUBLIC(QWIdleInhibitorV1)
+    QWSignalConnector sc;
+};
+QHash<void*, QWIdleInhibitorV1*> QWIdleInhibitorV1Private::map;
+
+void QWIdleInhibitorV1Private::on_destroy(void *)
+{
+    destroy();
+    m_handle = nullptr;
+    delete q_func();
+}
+
+QWIdleInhibitorV1::QWIdleInhibitorV1(wlr_idle_inhibitor_v1 *handle, bool isOwner)
+    : QObject(nullptr)
+    , QWObject(*new QWIdleInhibitorV1Private(handle, isOwner, this))
+{
+
+}
+
+QWIdleInhibitorV1 *QWIdleInhibitorV1::get(wlr_idle_inhibitor_v1 *handle)
+{
+    return QWIdleInhibitorV1Private::map.value(handle);
+}
+
+QWIdleInhibitorV1 *QWIdleInhibitorV1::from(wlr_idle_inhibitor_v1 *handle)
+{
+    if (auto o = get(handle))
+        return o;
+    return new QWIdleInhibitorV1(handle, false);
+}
+
+QW_END_NAMESPACE
---
+++ src/types/qwidleinhibitv1.h
@@ -0,0 +1,60 @@
+// Copyright (C) 2023 rewine <luhongxu@deepin.org>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QObject>
+
+struct wlr_idle_inhibit_manager_v1;
+struct wlr_idle_inhibitor_v1;
+
+QW_BEGIN_NAMESPACE
+
+class QWDisplay;
+class QWIdleInhibitManagerV1Private;
+class QW_EXPORT QWIdleInhibitManagerV1 : public QObject, public QWObject
+{
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWIdleInhibitManagerV1)
+public:
+    inline wlr_idle_inhibit_manager_v1 *handle() const {
+        return QWObject::handle<wlr_idle_inhibit_manager_v1>();
+    }
+
+    static QWIdleInhibitManagerV1 *get(wlr_idle_inhibit_manager_v1 *handle);
+    static QWIdleInhibitManagerV1 *from(wlr_idle_inhibit_manager_v1 *handle);
+    static QWIdleInhibitManagerV1 *create(QWDisplay *display);
+
+Q_SIGNALS:
+    void beforeDestroy(QWIdleInhibitManagerV1 *self);
+    void newInhibitor();
+
+private:
+    QWIdleInhibitManagerV1(wlr_idle_inhibit_manager_v1 *handle, bool isOwner);
+    ~QWIdleInhibitManagerV1() = default;
+};
+
+class QWIdleInhibitorV1Private;
+class QW_EXPORT QWIdleInhibitorV1 : public QObject, public QWObject
+{
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWIdleInhibitorV1)
+public:
+    inline wlr_idle_inhibitor_v1 *handle() const {
+        return QWObject::handle<wlr_idle_inhibitor_v1>();
+    }
+
+    static QWIdleInhibitorV1 *get(wlr_idle_inhibitor_v1 *handle);
+    static QWIdleInhibitorV1 *from(wlr_idle_inhibitor_v1 *handle);
+
+Q_SIGNALS:
+    void beforeDestroy(QWIdleInhibitorV1 *self);
+
+private:
+    QWIdleInhibitorV1(wlr_idle_inhibitor_v1 *handle, bool isOwner);
+    ~QWIdleInhibitorV1() = default;
+};
+
+QW_END_NAMESPACE
+
