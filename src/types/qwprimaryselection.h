ca4bc62e12ecb01a384bed8b71583d09f22ebb74
Zhang Dingyuan <zhangdingyuan@deepin.org>
2023-06-21 21:01:14 +0800

    Implement QWPrimarySelection from wlr_primary_selection

---
+++ src/types/qwprimaryselection.h
@@ -0,0 +1,42 @@
+// Copyright (C) 2023 Dingyuan Zhang <zhangdingyuan@uniontech.com>.
+// SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+
+#pragma once
+
+#include <qwglobal.h>
+#include <QObject>
+
+struct wlr_primary_selection_source_impl;
+struct wlr_primary_selection_source;
+struct wlr_seat_client;
+
+QW_BEGIN_NAMESPACE
+
+class QWSeat;
+class QWPrimarySelectionSourcePrivate;
+class QW_EXPORT QWPrimarySelectionSource : public QObject, public QWObject
+{
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWPrimarySelectionSource)
+public:
+    inline wlr_primary_selection_source *handle() const {
+        return QWObject::handle<wlr_primary_selection_source>();
+    }
+
+    static QWPrimarySelectionSource *get(wlr_primary_selection_source *handle);
+    static QWPrimarySelectionSource *from(wlr_primary_selection_source *handle);
+
+    void init(const wlr_primary_selection_source_impl *impl);
+    void send(const char *mime_type, int fd);
+    void requestSetPrimarySelection(QWSeat *seat, wlr_seat_client *client, uint32_t serial);
+    void setPrimarySelection(QWSeat *seat, uint32_t serial);
+
+Q_SIGNALS:
+    void beforeDestroy(QWPrimarySelectionSource *self);
+
+private:
+    QWPrimarySelectionSource(wlr_primary_selection_source *handle, bool isOwner);
+};
+
+QW_END_NAMESPACE
+
