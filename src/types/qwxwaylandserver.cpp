bc317edf6f9da380c2b13c0a5fd2ff3448d99932
justforlxz <justforlxz@gmail.com>
2023-06-28 11:48:48 +0800

    Implement wlr_xwayland_server from QWXWaylandServer

--- src/types/qwxwaylandserver.cpp
+++ src/types/qwxwaylandserver.cpp
@@ -2,33 +2,100 @@
 // SPDX-License-Identifier: Apache-2.0 OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
 #include "qwxwaylandserver.h"
+#include "qwdisplay.h"
+#include "util/qwsignalconnector.h"
 
-#include <qwdisplay.h>
+#include <QHash>
 
 extern "C" {
 #include <wlr/xwayland/server.h>
 }
 
 QW_BEGIN_NAMESPACE
-
-QWXWaylandServer* QWXWaylandServer::from(wlr_xwayland_server* xwayland)
+class QWXWaylandServerPrivate : public QWObjectPrivate
 {
-    return reinterpret_cast<QWXWaylandServer*>(xwayland);
+public:
+    QWXWaylandServerPrivate(wlr_xwayland_server *handle, bool isOwner, QWXWaylandServer *qq)
+        : QWObjectPrivate(handle, isOwner, qq)
+    {
+        Q_ASSERT(!map.contains(handle));
+        map.insert(handle, qq);
+        sc.connect(&handle->events.destroy, this, &QWXWaylandServerPrivate::on_destroy);
+#if WLR_VERSION_MINOR > 17
+        sc.connect(&handle->events.start, this, &QWXWaylandServerPrivate::on_start);
+        sc.connect(&handle->events.ready, this, &QWXWaylandServerPrivate::on_ready);
+#endif
+    }
+    ~QWXWaylandServerPrivate() {
+        if (!m_handle)
+            return;
+        destroy();
+        if (isHandleOwner)
+            wlr_xwayland_server_destroy(q_func()->handle());
+    }
+
+    inline void destroy() {
+        Q_ASSERT(m_handle);
+        Q_ASSERT(map.contains(m_handle));
+        Q_EMIT q_func()->beforeDestroy(q_func());
+        map.remove(m_handle);
+        sc.invalidate();
+    }
+
+    void on_destroy(void *);
+#if WLR_VERSION_MINOR > 17
+    void on_start(void *);
+    void on_ready(void *);
+#endif
+
+    static QHash<void*, QWXWaylandServer*> map;
+    QW_DECLARE_PUBLIC(QWXWaylandServer)
+    QWSignalConnector sc;
+};
+QHash<void*, QWXWaylandServer*> QWXWaylandServerPrivate::map;
+
+void QWXWaylandServerPrivate::on_destroy(void *)
+{
+    destroy();
+    m_handle = nullptr;
+    delete q_func();
 }
 
-QWXWaylandServer* QWXWaylandServer::create(QWDisplay *display, wlr_xwayland_server_options *options)
+#if WLR_VERSION_MINOR > 17
+void QWXWaylandServerPrivate::on_start(void *)
 {
-    return from(wlr_xwayland_server_create(display->handle(), options));
+    Q_EMIT q_func()->start();
 }
 
-void QWXWaylandServer::destroy()
+void QWXWaylandServerPrivate::on_ready(void *)
 {
-    wlr_xwayland_server_destroy(handle());
+    Q_EMIT q_func()->ready();
+}
+#endif
+
+QWXWaylandServer::QWXWaylandServer(wlr_xwayland_server *handle, bool isOwner)
+    : QObject(nullptr)
+    , QWObject(*new QWXWaylandServerPrivate(handle, isOwner, this))
+{
+
 }
 
-wlr_xwayland_server* QWXWaylandServer::handle() const
+QWXWaylandServer *QWXWaylandServer::get(wlr_xwayland_server *handle)
 {
-    return reinterpret_cast<wlr_xwayland_server*>(const_cast<QWXWaylandServer*>(this));
+    return QWXWaylandServerPrivate::map.value(handle);
+}
+
+QWXWaylandServer *QWXWaylandServer::from(wlr_xwayland_server *handle)
+{
+    if (auto o = get(handle))
+        return o;
+    return new QWXWaylandServer(handle, false);
+}
+
+QWXWaylandServer *QWXWaylandServer::create(QWDisplay *display, wlr_xwayland_server_options *options)
+{
+    auto *handle = wlr_xwayland_server_create(display->handle(), options);
+    return handle ? new QWXWaylandServer(handle, true) : nullptr;
 }
 
 QW_END_NAMESPACE
--- src/types/qwxwaylandserver.h
+++ src/types/qwxwaylandserver.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <qwglobal.h>
+#include <QObject>
 
 struct wlr_xwayland_server;
 struct wlr_xwayland_server_options;
@@ -11,18 +12,31 @@ struct wlr_xwayland_server_options;
 QW_BEGIN_NAMESPACE
 
 class QWDisplay;
-class QWXWaylandServer
+class QWXWaylandServerPrivate;
+class QW_EXPORT QWXWaylandServer : public QObject, public QWObject
 {
+    Q_OBJECT
+    QW_DECLARE_PRIVATE(QWXWaylandServer)
 public:
-    QWXWaylandServer() = delete;
-    ~QWXWaylandServer() = delete;
+    ~QWXWaylandServer() override = default;
 
-    static QWXWaylandServer* from(wlr_xwayland_server* xwayland);
-    static QWXWaylandServer* create(QWDisplay *display, wlr_xwayland_server_options *options);
+    inline wlr_xwayland_server *handle() const {
+        return QWObject::handle<wlr_xwayland_server>();
+    }
 
-    void destroy();
-    wlr_xwayland_server* handle() const;
+    static QWXWaylandServer *get(wlr_xwayland_server *handle);
+    static QWXWaylandServer *from(wlr_xwayland_server *handle);
+    static QWXWaylandServer *create(QWDisplay *display, wlr_xwayland_server_options *options);
+
+Q_SIGNALS:
+    void beforeDestroy(QWXWaylandServer *self);
+#if WLR_VERSION_MINOR > 17
+    void start();
+    void ready();
+#endif
+
+private:
+    QWXWaylandServer(wlr_xwayland_server *handle, bool isOwner);
 };
 
 QW_END_NAMESPACE
-
