09470bbb9720374bc182dd4bdb8ccb913fef9187
lbwtw <xiaoyaobing@uniontech.com>
2024-06-20 17:48:32 +0800

    add QWGammaControl

--- src/types/qwgammacontorlv1.cpp
+++ src/types/qwgammacontorlv1.cpp
@@ -3,6 +3,7 @@
 
 #include "qwgammacontorlv1.h"
 #include "qwdisplay.h"
+#include "qwoutput.h"
 #include "private/qwglobal_p.h"
 
 #include <QHash>
@@ -56,6 +57,31 @@ QWGammaControlManagerV1 *QWGammaControlManagerV1::create(QWDisplay *display)
     if (!handle)
         return nullptr;
     return new QWGammaControlManagerV1(handle, true);
+}
+
+wlr_gamma_control_v1 *QWGammaControl::handle() const
+{
+    return reinterpret_cast<wlr_gamma_control_v1*>(const_cast<QWGammaControl*>(this));
+}
+
+QWGammaControl *QWGammaControl::from(wlr_gamma_control_v1 *handle)
+{
+    return reinterpret_cast<QWGammaControl*>(handle);
+}
+
+void QWGammaControl::sendFailedAndDestroy()
+{
+    wlr_gamma_control_v1_send_failed_and_destroy(handle());
+}
+
+bool QWGammaControl::gammaControlApply(wlr_output_state *output_state)
+{
+    return wlr_gamma_control_v1_apply(handle(), output_state);
+}
+
+QWGammaControl *QWGammaControlManagerV1::getControl(QWOutput *output)
+{
+    return QWGammaControl::from(wlr_gamma_control_manager_v1_get_control(handle(), output->handle()));
 }
 
 QW_END_NAMESPACE
--- src/types/qwgammacontorlv1.h
+++ src/types/qwgammacontorlv1.h
@@ -6,13 +6,31 @@
 #include <qwglobal.h>
 #include <QObject>
 
+struct wlr_output_state;
+struct wlr_gamma_control_v1;
 struct wlr_gamma_control_manager_v1;
 struct wlr_gamma_control_manager_v1_set_gamma_event;
 
 QW_BEGIN_NAMESPACE
 
+class QWOutput;
 class QWDisplay;
 class QWGammaControlManagerV1Private;
+
+class QW_EXPORT QWGammaControl
+{
+public:
+    QWGammaControl() = delete;
+    ~QWGammaControl() = delete;
+
+    wlr_gamma_control_v1 *handle() const;
+
+    static QWGammaControl *from(wlr_gamma_control_v1 *handle);
+
+    void sendFailedAndDestroy();
+    bool gammaControlApply(struct wlr_output_state *output_state);
+};
+
 class QW_EXPORT QWGammaControlManagerV1 : public QWWrapObject
 {
     Q_OBJECT
@@ -25,6 +43,8 @@ public:
     static QWGammaControlManagerV1 *get(wlr_gamma_control_manager_v1 *handle);
     static QWGammaControlManagerV1 *from(wlr_gamma_control_manager_v1 *handle);
     static QWGammaControlManagerV1 *create(QWDisplay *display);
+
+    QWGammaControl *getControl(QWOutput *output);
 
 Q_SIGNALS:
     void gammaChanged(wlr_gamma_control_manager_v1_set_gamma_event *event);
