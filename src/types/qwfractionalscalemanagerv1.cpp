775dc2677263dfea1236f965e7e0a32df17f659c
lbwtw <xiaoyaobing@uniontech.com>
2024-07-01 17:44:03 +0800

    make types part header only

--- src/types/qwfractionalscalemanagerv1.cpp
+++
@@ -1,7 +0,0 @@
-#include "qwfractionalscalemanagerv1.h"
-#include "qwdisplay.h"
-#include "qwcompositor.h"
-#include "private/qwglobal_p.h"
-#include <QHash>
-extern "C" {
-#include <wlr/types/wlr_fractional_scale_v1.h>
--- src/types/qwfullscreenshellv1.cpp
+++
@@ -1,7 +0,0 @@
-#include "qwfullscreenshellv1.h"
-#include "qwdisplay.h"
-#include "private/qwglobal_p.h"
-#include <QHash>
-extern "C" {
-#include <wlr/types/wlr_fullscreen_shell_v1.h>
-QW_SIGNAL(present_surface, arguments?)
