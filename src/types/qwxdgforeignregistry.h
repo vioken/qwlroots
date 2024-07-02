bccb4f1cd2bf88d57be853be7a18d27535b10fdc
pengwenhao <pengwenhao@uniontech.com>
2024-07-02 14:32:15 +0800

    refactor: make some header only
    Signed-off-by: pengwenhao <pengwenhao@uniontech.com>

--- src/types/qwxdgforeignregistry.h
+++ src/types/qwxdgforeignregistry.h
@@ -3,58 +3,26 @@
 
 #pragma once
 
-#include <qwglobal.h>
-#include <QObject>
-
-struct wlr_xdg_foreign_registry;
-struct wlr_xdg_foreign_exported;
+#include <qwobject.h>
 
 QW_BEGIN_NAMESPACE
 
-class QWDisplay;
-class QWXdgForeignExported;
-class QWXdgForeignRegistryPrivate;
-class QW_EXPORT QWXdgForeignRegistry : public QWWrapObject
+class QW_CLASS_OBJECT(xdg_foreign_registry)
 {
+    QW_OBJECT
     Q_OBJECT
-    QW_DECLARE_PRIVATE(QWXdgForeignRegistry)
 public:
-    static QWXdgForeignRegistry *create(QWDisplay *display);
-    static QWXdgForeignRegistry *from(wlr_xdg_foreign_registry *handle);
-    static QWXdgForeignRegistry *get(wlr_xdg_foreign_registry *handle);
-
-    QWXdgForeignExported *findByHandle(const char *handle);
-    inline wlr_xdg_foreign_registry *handle() const {
-        return QWObject::handle<wlr_xdg_foreign_registry>();
-    }
-
-private:
-    explicit QWXdgForeignRegistry(wlr_xdg_foreign_registry *handle, bool isOwner);
-    ~QWXdgForeignRegistry() override = default;
+    QW_FUNC_STATIC(xdg_foreign_registry, create)
+    QW_FUNC_MEMBER(xdg_foreign_registry, find_by_handle)
 };
 
-class QWXdgForeignExportedPrivate;
-class QW_EXPORT QWXdgForeignExported : public QWWrapObject
+class QW_CLASS_OBJECT(xdg_foreign_exported)
 {
+    QW_OBJECT
     Q_OBJECT
-    QW_DECLARE_PRIVATE(QWXdgForeignExported)
 public:
-    static QWXdgForeignExported *from(wlr_xdg_foreign_exported *handle);
-    static QWXdgForeignExported *get(wlr_xdg_foreign_exported *handle);
-
-    inline wlr_xdg_foreign_exported *handle() const {
-        return QWObject::handle<wlr_xdg_foreign_exported>();
-    }
-
-    bool init(QWXdgForeignRegistry *registry);
-    void finish();
-
-protected:
-    QWXdgForeignExported(QWXdgForeignExportedPrivate &dd);
-    virtual ~QWXdgForeignExported() override = default;
-
-private:
-    explicit QWXdgForeignExported(wlr_xdg_foreign_exported *handle, bool isOwner);
+    QW_FUNC_MEMBER(xdg_foreign_exported, init)
+    QW_FUNC_MEMBER(xdg_foreign_exported, finish)
 };
 
 QW_END_NAMESPACE
