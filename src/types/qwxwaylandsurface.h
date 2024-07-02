e734f627f74b724651e231a85fa1f1517d2bb217
Zhang Dingyuan <zhangdingyuan@deepin.org>
2024-07-02 17:19:28 +0800

    refactor: to header only

--- src/types/qwxwaylandsurface.h
+++ src/types/qwxwaylandsurface.h
@@ -4,73 +4,59 @@
 
 #pragma once
 
-#include <qwglobal.h>
-#include <QObject>
+#include <qwobject.h>
 
-typedef uint32_t wlr_xwayland_icccm_input_model_t;
-typedef uint32_t xcb_stack_mode;
-
-struct wlr_xwayland_surface;
-struct wlr_xwayland_surface_configure_event;
-struct wlr_xwayland_resize_event;
-struct wlr_xwayland_minimize_event;
-
-QT_BEGIN_NAMESPACE
-class QRect;
-QT_END_NAMESPACE
+extern "C" {
+#include <xcb/xproto.h>
+#include <math.h>
+#define class _class
+#include <wlr/xwayland/xwayland.h>
+#undef class
+}
 
 QW_BEGIN_NAMESPACE
 
-class QWSurface;
-class QWXWaylandSurfacePrivate;
-class QW_EXPORT QWXWaylandSurface : public QWWrapObject
+class QW_CLASS_OBJECT(xwayland_surface)
 {
+    QW_OBJECT
     Q_OBJECT
-    QW_DECLARE_PRIVATE(QWXWaylandSurface)
+
 public:
-    static QWXWaylandSurface *get(wlr_xwayland_surface* handle);
-    static QWXWaylandSurface* from(wlr_xwayland_surface* handle);
-    static QWXWaylandSurface *tryFromWlrSurface(QWSurface *surface);
+    QW_SIGNAL(request_configure, wlr_xwayland_surface_configure_event *)
+    QW_SIGNAL(request_move)
+    QW_SIGNAL(request_resize, wlr_xwayland_resize_event *)
+    QW_SIGNAL(request_minimize, wlr_xwayland_minimize_event *)
+    QW_SIGNAL(request_maximize)
+    QW_SIGNAL(request_fullscreen)
+    QW_SIGNAL(request_activate)
+    QW_SIGNAL(set_title)
+    QW_SIGNAL(set_class)
+    QW_SIGNAL(set_role)
+    QW_SIGNAL(set_parent)
+    QW_SIGNAL(set_startup_id)
+    QW_SIGNAL(set_window_type)
+    QW_SIGNAL(set_hints)
+    QW_SIGNAL(set_decorations)
+    QW_SIGNAL(set_override_redirect)
+    QW_SIGNAL(set_geometry)
+    QW_SIGNAL(ping_timeout)
+    QW_SIGNAL(associate)
+    QW_SIGNAL(dissociate)
 
-    wlr_xwayland_surface* handle() const;
-    void activate(bool activated);
-    void restack(QWXWaylandSurface *sibling, xcb_stack_mode mode);
-    void configure(const QRect &rect);
-    void close();
-    void setWithdrawn(bool withdrawn);
-    void setMinimized(bool minimized);
-    void setMaximized(bool maximized);
-    void setFullscreen(bool fullscreen);
-    void ping();
-    bool xwaylandOrSurfaceWantsFocus() const;
-    wlr_xwayland_icccm_input_model_t icccmInputModel() const;
+public:
+    QW_FUNC_STATIC(xwayland_surface, try_from_wlr_surface)
 
-Q_SIGNALS:
-    void requestConfigure(wlr_xwayland_surface_configure_event *event);
-    void requestMove();
-    void requestResize(wlr_xwayland_resize_event *event);
-    void requestMinimize(wlr_xwayland_minimize_event *event);
-    void requestMaximize();
-    void requestFullscreen();
-    void requestActivate();
-    void titleChanged();
-    void classChanged();
-    void roleChanged();
-    void parentChanged();
-    void startupIdChanged();
-    void windowTypeChanged();
-    void hintsChanged();
-    void decorationsChanged();
-    void overrideRedirectChanged();
-    void geometryChanged();
-    void pingTimeout();
-    void associate();
-    void dissociate();
-    void strutPartialChanged();
-
-private:
-    QWXWaylandSurface(wlr_xwayland_surface *handle);
-    ~QWXWaylandSurface() override = default;
+    QW_FUNC_MEMBER(xwayland_surface, activate)
+    QW_FUNC_MEMBER(xwayland_surface, restack)
+    QW_FUNC_MEMBER(xwayland_surface, configure)
+    QW_FUNC_MEMBER(xwayland_surface, close)
+    QW_FUNC_MEMBER(xwayland_surface, set_withdrawn)
+    QW_FUNC_MEMBER(xwayland_surface, set_minimized)
+    QW_FUNC_MEMBER(xwayland_surface, set_maximized)
+    QW_FUNC_MEMBER(xwayland_surface, set_fullscreen)
+    QW_FUNC_MEMBER(xwayland_surface, ping)
+    QW_FUNC_MEMBER(xwayland_surface, icccm_input_model)
+    QW_FUNC_MEMBER(xwayland_surface, xwayland_or_surface_wants_focus)
 };
 
 QW_END_NAMESPACE
