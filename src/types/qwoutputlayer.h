c93034d83057912a892c13a849a823042ab1062e
rewine <luhongxu@deepin.org>
2024-07-31 15:41:09 +0800

    Fix create of qw_output_layer is not static

--- src/types/qwoutputlayer.h
+++ src/types/qwoutputlayer.h
@@ -20,7 +20,7 @@ class QW_CLASS_OBJECT(output_layer)
     QW_SIGNAL(feedback, wlr_output_layer_feedback_event*)
 
 public:
-    QW_ALWAYS_INLINE qw_output_layer *create(wlr_output *output) {
+    QW_ALWAYS_INLINE static qw_output_layer *create(wlr_output *output) {
         auto layer = wlr_output_layer_create(output);
         if (layer)
             return nullptr;
