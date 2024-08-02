be21d50d70b1107a17f35aa648bb19abf1b40737
rewine <luhongxu@deepin.org>
2024-08-02 14:37:02 +0800

    Add missing binding for qwdisplay disable pch by default

--- src/qwdisplay.h
+++ src/qwdisplay.h
@@ -32,22 +32,30 @@ class QW_EXPORT qw_display : public qw_object<wl_display, qw_display>
 public:
     explicit qw_display() : qw_object(wl_display_create(), true) { }
 
+    QW_DISPLAY_FUNC_MEMBER(get_event_loop, wl_event_loop*)
     QW_DISPLAY_FUNC_MEMBER(add_socket, int, const char *name)
     QW_DISPLAY_FUNC_MEMBER(add_socket_fd, int, int sock_fd)
     QW_DISPLAY_FUNC_MEMBER(add_socket_auto, const char*)
     QW_DISPLAY_FUNC_MEMBER(run, void)
     QW_DISPLAY_FUNC_MEMBER(terminate, void)
-    QW_DISPLAY_FUNC_MEMBER(get_event_loop, wl_event_loop*)
+    QW_DISPLAY_FUNC_MEMBER(flush_clients, void)
+    QW_DISPLAY_FUNC_MEMBER(destroy_clients, void)
+    QW_DISPLAY_FUNC_MEMBER(get_serial, uint32_t)
+    QW_DISPLAY_FUNC_MEMBER(next_serial, uint32_t)
+    QW_DISPLAY_FUNC_MEMBER(set_global_filter, void, wl_display_global_filter_func_t, void*)
+    QW_DISPLAY_FUNC_MEMBER(init_shm, int)
+    QW_DISPLAY_FUNC_MEMBER(add_shm_format, uint32_t*, uint32_t)
+    QW_DISPLAY_FUNC_MEMBER(add_protocol_logger, wl_protocol_logger*, wl_protocol_logger_func_t, void*)
 
     void start(QThread *thread) {
-        auto loop = wl_display_get_event_loop(*this);
+        auto loop = this->get_event_loop();
         int fd = wl_event_loop_get_fd(loop);
 
         auto processWaylandEvents = [this, loop] () {
             int ret = wl_event_loop_dispatch(loop, 0);
             if (ret)
                 fprintf(stderr, "wl_event_loop_dispatch error: %d\n", ret);
-            wl_display_flush_clients(*this);
+            this->flush_clients();
         };
 
         auto sockNot = new QSocketNotifier(fd, QSocketNotifier::Read, this);
